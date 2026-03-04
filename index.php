14f535c5d3421e3ca0cd7b673e55e0a726b42a16
saadalgaashani <saad.algaashani@gmail.com>
2026-03-04 16:33:53 +0100

    groote update

--- index.php
+++ index.php
@@ -4,20 +4,30 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Escape Room</title>
+  <title>Spy Escape Room</title>
   <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
 
-  <h1>Welkom</h1>
-  <p>De button hieronder is alleen voor testdoeleinden bij het opstarten van het project. Deze pagina is bedoeld voor de
-    uitleg van jullie escape room.</p>
-  <p>Via deze pagina gaat een team naar de aanmeldpagina</p>
+ <div class="home">
+  <div class="text">
+    <h1>Welkom bij de Spy Escap Room!</h1>
 
+    <p class="description">
+      De escape room speelt zich af <br> in een oud en verlaten huis waar een spion zich verstopt nadat hij geheime informatie heeft gestolen. <br>
+      De politie heeft hem gevolgd en staat nu buiten het huis te wachten om hem te arresteren. <br>
+      De speler moet binnen 5 minuten verschillende puzzels oplossen om een geheime uitgang te vinden en te ontsnappen.
+      <br> Voordat de politie binnenkomt en voordat het huis gevaarlijk wordt door mysterieuze krachten.
+    </p>
 
-  <button><a href="./rooms/room_1.php">Klik hier voor een demonstratie van kamer
-      1</a></button>
+    <p>Via deze pagina gaat een team naar de Escap room</p>
+
+    <a class="play-btn" href="./rooms/room_1.php">NU Spelen!</a>
+  </div>
+
+  <img src="thumbnai.png" class="thumbnail" alt="Spy Escape Room">
+</div>
 
 </body>
 
